aa96da93e266251f31cde8e836916eaa4cc6b4cd
JuileeRaut <juilee.megavision@gmail.com>
2020-04-30 17:29:15 +0530

    area

--- app/src/main/java/com/example/feedbackapplication/adminlogin/SelectArea.java
+++ app/src/main/java/com/example/feedbackapplication/adminlogin/SelectArea.java
@@ -2,6 +2,7 @@ package com.example.feedbackapplication.adminlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -21,6 +22,7 @@ import com.example.feedbackapplication.FeedbackActivity;
 import com.example.feedbackapplication.R;
 import com.example.feedbackapplication.database.DatabaseHelper;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
@@ -31,9 +33,12 @@ public class SelectArea extends BaseActivity {
 
     //    DatabaseHelper databaseHelper;
 //    SQLiteDatabase sqLiteDatabase;
-    String AreaName = "Cafeteria|Washroom";
+    String AreaName ;//= "Cafeteria|Washroom";
     ArrayList<String> areaList;
     LinearLayout linearLayout, linearLayout1;
+    private byte[] img = null;
+    private byte[] img1 = null;
+    int imagecount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,42 @@ public class SelectArea extends BaseActivity {
 //        setContentView(R.layout.activity_select_area);
 //        databaseHelper = new DatabaseHelper(this);
 //        sqLiteDatabase = databaseHelper.getWritableDatabase();
+        imagecount = dbh.totalimage_count();
+        if (imagecount == 0) {
+            try {
+                Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.cafeteria);
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                b.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                img = bos.toByteArray();
+                //myDB = new DatabaseHelper(this);
+
+                ContentValues cv = new ContentValues();
+                cv.put("Icon_Name", "Cafeteria");
+                cv.put("Icon_value", img);
+                cv.put("Icon_Type", "image");
+//            cv.put("Area_Name", "Cafeteria");
+
+                sqLiteDatabase.insert("feedback_admin_icondetails", null, cv);
+
+
+                Bitmap b1 = BitmapFactory.decodeResource(getResources(), R.drawable.washroom);
+                ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
+                b1.compress(Bitmap.CompressFormat.PNG, 100, bos1);
+                img1 = bos1.toByteArray();
+                //myDB = new DatabaseHelper(this);
+
+                ContentValues cv1 = new ContentValues();
+                cv1.put("Icon_Name", "Washroom");
+                cv1.put("Icon_value", img1);
+                cv1.put("Icon_Type", "image");
+//            cv1.put("Area_Name", "Cafeteria");
+
+                sqLiteDatabase.insert("feedback_admin_icondetails", null, cv1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         linearLayout = new LinearLayout(this);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT);
         linearLayout.setLayoutParams(params);
@@ -58,7 +99,7 @@ public class SelectArea extends BaseActivity {
         linearLayout1.setOrientation(LinearLayout.HORIZONTAL);
         linearLayout1.setGravity(Gravity.CENTER);
         areaList = new ArrayList<String>();
-       /* try {
+        try {
             SQLiteDatabase db1 = dbh.getWritableDatabase();
 
             Cursor cursor1 = db1.rawQuery("Select Area_Name from store_setting ;", null);
@@ -73,7 +114,7 @@ public class SelectArea extends BaseActivity {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
         if (AreaName.contains("|")) {
             String[] areas = AreaName.split("\\|");
             for (int i = 0; i < areas.length; i++) {
@@ -86,14 +127,14 @@ public class SelectArea extends BaseActivity {
         for (int j = 0; j < areaList.size(); j++) {
 
             String area = areaList.get(j).toString();
-
-            if (area.equalsIgnoreCase("Cafeteria")) {
+            linearLayout1.addView(imageView(j + 1, area));
+            /*if (area.equalsIgnoreCase("Cafeteria")) {
                 linearLayout1.addView(imageView(j + 1, area));
 
             } else if (area.equalsIgnoreCase("Washroom")) {
                 linearLayout1.addView(imageView(j + 1, area));
 
-            }
+            }*/
 
 
         }
@@ -111,30 +152,30 @@ public class SelectArea extends BaseActivity {
         imageView.setLayoutParams(params);
         Bitmap b = null;
 
-        if (imgvalue.equalsIgnoreCase("Cafeteria")) {
-            imageView.setBackgroundResource(R.drawable.cafeteria);
-        } else if (imgvalue.equalsIgnoreCase("Washroom")) {
-            imageView.setBackgroundResource(R.drawable.washroom);
+           /* if (imgvalue.equalsIgnoreCase("Cafeteria")) {
+                imageView.setBackgroundResource(R.drawable.cafeteria);
+            } else if (imgvalue.equalsIgnoreCase("Washroom")) {
+                imageView.setBackgroundResource(R.drawable.washroom);
+            }*/
+        byte[] image_str = dbh.readDataIcon(strvalue);
+        ;
+        try {
+            b = BitmapFactory.decodeByteArray(image_str, 0, image_str.length);
+
+            imageView.setImageBitmap(Bitmap.createScaledBitmap(b, 600, 200, true));
+        } catch (Exception e) {
+
+            e.printStackTrace();
         }
-//        byte[] image_str = dbh.readDataIcon(strvalue);
-//        ;
-//        try {
-//            b = BitmapFactory.decodeByteArray(image_str, 0, image_str.length);
-//
-//            imageView.setImageBitmap(Bitmap.createScaledBitmap(b, 100, 100, true));
-//        } catch (Exception e) {
-//
-//            e.printStackTrace();
-//        }
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (imgvalue.equalsIgnoreCase("Cafeteria")) {
-                   Intent i=new Intent(SelectArea.this,FeedbackActivity.class);
-                   startActivity(i);
+                    Intent i = new Intent(SelectArea.this, FeedbackActivity.class);
+                    startActivity(i);
                 } else if (imgvalue.equalsIgnoreCase("Washroom")) {
-                    Intent i=new Intent(SelectArea.this,FeedbackService.class);
+                    Intent i = new Intent(SelectArea.this, FeedbackService.class);
                     startActivity(i);
                 }
             }
